0a241d2e4139937ffbfdf5e6c660f87784d977c2
lue-bird <lue.the.bird@gmail.com>
2025-08-21 12:34:45 +0200

    update example-hello-world

--- example-hello-world/src/main.rs
+++ example-hello-world/src/main.rs
@@ -2,6 +2,6 @@ mod elm;
 
 pub fn main() {
     let allocator: bumpalo::Bump = bumpalo::Bump::new();
-    let greetings: elm::StringString = elm::hello_greet(&allocator, "elmstacean 🦀");
+    let greetings: &str = elm::hello_greet(&allocator, "elmstacean 🦀");
     println!("{:?}", elm::string_to_upper(&allocator, greetings));
 }
