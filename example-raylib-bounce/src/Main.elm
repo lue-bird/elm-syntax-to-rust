f7ca512eed031a3bd5e3c0f62ed69551cd239c3d
lue-bird <lue.the.bird@gmail.com>
2025-08-29 12:54:10 +0200

    add example raylib-bounce

---
+++ example-raylib-bounce/src/Main.elm
@@ -0,0 +1,138 @@
+module Main exposing (ElementToRender(..), Interface(..), State, initWindow, stateToInterface)
+
+import Color exposing (Color)
+
+
+type State
+    = StateRunning
+        { y : Float
+        , yVelocity : Float
+        }
+    | StateCloseWindow
+
+
+windowHeight : Int
+windowHeight =
+    600
+
+
+windowWidth : Int
+windowWidth =
+    800
+
+
+initWindow :
+    { windowWidth : Int
+    , windowHeight : Int
+    , windowTitle : String
+    , state : State
+    }
+initWindow =
+    { windowWidth = windowWidth
+    , windowHeight = windowHeight
+    , windowTitle = """rust 🤝 elm 🤝 raylib"""
+    , state =
+        StateRunning
+            { y = 0.3 * ((windowHeight |> Basics.toFloat) - characterHeight)
+            , yVelocity = 0
+            }
+    }
+
+
+type Interface state
+    = InterfaceRender
+        { clearColor : Color
+        , elements : List ElementToRender
+        }
+    | InterfaceQuit
+    | InterfaceOnSimulationTick (() -> state)
+    | InterfaceOnQuit (() -> state)
+
+
+type ElementToRender
+    = FilledRectangleToRender FilledRectangleToRender
+
+
+type alias FilledRectangleToRender =
+    RecordWithoutConstructorFunction
+        { left : Float
+        , top : Float
+        , width : Float
+        , height : Float
+        , color : Color
+        }
+
+
+stateToInterface : State -> List (Interface State)
+stateToInterface state =
+    case state of
+        StateCloseWindow ->
+            [ InterfaceQuit ]
+
+        StateRunning running ->
+            [ InterfaceOnQuit (\() -> StateCloseWindow)
+            , InterfaceRender
+                { clearColor = Color.rgba 0.047 0 0 0
+                , elements =
+                    [ let
+                        wiggleRoom : Float
+                        wiggleRoom =
+                            min 2 (abs running.yVelocity * 0.1)
+                                * (sqrt (characterHeight * characterWidth) / 2)
+
+                        squish : Float
+                        squish =
+                            Basics.clamp
+                                0
+                                1
+                                ((running.y - ((windowHeight |> Basics.toFloat) - (characterHeight + wiggleRoom)))
+                                    / wiggleRoom
+                                )
+                      in
+                      FilledRectangleToRender
+                        { left =
+                            ((windowWidth |> Basics.toFloat) / 2)
+                                - (characterWidth / 2)
+                                - ((wiggleRoom * squish) / 2)
+                        , top = running.y
+                        , width = characterWidth + wiggleRoom * squish
+                        , height = characterHeight - wiggleRoom * squish
+                        , color = Color.rgb255 0 0 100
+                        }
+                    ]
+                }
+            , InterfaceOnSimulationTick
+                (\() ->
+                    let
+                        updatedYVelocity : Float
+                        updatedYVelocity =
+                            if running.y >= (windowHeight |> Basics.toFloat) - characterHeight then
+                                -running.yVelocity
+
+                            else
+                                running.yVelocity + 0.1
+                    in
+                    StateRunning
+                        { y = running.y + updatedYVelocity
+                        , yVelocity = updatedYVelocity
+                        }
+                )
+            ]
+
+
+characterHeight : Float
+characterHeight =
+    100
+
+
+characterWidth : Float
+characterWidth =
+    100
+
+
+
+--
+
+
+type alias RecordWithoutConstructorFunction a =
+    a
---
+++ example-raylib-bounce/src/main.rs
@@ -0,0 +1,74 @@
+mod elm;
+
+fn main() {
+    let ((mut raylib_handle, raylib_thread), mut elm_state) = {
+        let allocator_for_init = bumpalo::Bump::new();
+        let elm_initialized = elm::main_init_window(&allocator_for_init);
+        (
+            raylib::init()
+                .size(
+                    elm_initialized.window_width as i32,
+                    elm_initialized.window_height as i32,
+                )
+                .title(&elm_initialized.window_title.to_string())
+                .build(),
+            elm_initialized.state,
+        )
+    };
+    // check if this limits the throughput on effects etc, if yes, replace
+    raylib::prelude::RaylibHandle::set_target_fps(&mut raylib_handle, 60);
+    'main_loop: loop {
+        let allocator_for_interface: bumpalo::Bump = bumpalo::Bump::new();
+        'interface_handling: for interface in
+            elm::main_state_to_interface(&allocator_for_interface, elm_state).into_iter()
+        {
+            match interface {
+                elm::MainInterface::InterfaceOnQuit(construct_new_state) => {
+                    if raylib_handle.window_should_close() {
+                        elm_state = construct_new_state(());
+                        break 'interface_handling;
+                    }
+                }
+                elm::MainInterface::InterfaceQuit => {
+                    break 'main_loop;
+                }
+                elm::MainInterface::InterfaceOnSimulationTick(construct_new_state) => {
+                    elm_state = construct_new_state(());
+                    break 'interface_handling;
+                }
+                elm::MainInterface::InterfaceRender(to_render) => {
+                    let mut raylib_renderer: raylib::prelude::RaylibDrawHandle =
+                        raylib_handle.begin_drawing(&raylib_thread);
+                    raylib::prelude::RaylibDraw::clear_background(
+                        &mut raylib_renderer,
+                        elm_color_to_raylib(to_render.clear_color),
+                    );
+                    for element_to_render in to_render.elements.into_iter() {
+                        match element_to_render {
+                            elm::MainElementToRender::FilledRectangleToRender(rectangle) => {
+                                raylib::prelude::RaylibDraw::draw_rectangle(
+                                    &mut raylib_renderer,
+                                    rectangle.left as i32,
+                                    rectangle.top as i32,
+                                    rectangle.width as i32,
+                                    rectangle.height as i32,
+                                    elm_color_to_raylib(rectangle.color),
+                                );
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn elm_color_to_raylib(elm_color: elm::ColorColor) -> raylib::prelude::Color {
+    let components = elm::color_to_rgba(elm_color);
+    raylib::prelude::Color::new(
+        (components.red * 255.0) as u8,
+        (components.green * 255.0) as u8,
+        (components.blue * 255.0) as u8,
+        (components.alpha * 255.0) as u8,
+    )
+}
